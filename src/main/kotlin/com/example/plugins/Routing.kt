95d3a0ef2cf1e7a679717530764b444be9735182
BlackHandMinsk <3058079@gmail.com>
2022-02-14 11:42:29 +0300

    All heroes endpoint

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -3,6 +3,7 @@ package com.example.plugins
 import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.application.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -10,5 +11,8 @@ fun Application.configureRouting(){
     routing {
        root()
         getAllHeroes()
+        static("/images"){
+            resources("images")
+        }
     }
 }
